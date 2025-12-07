892ab2514b0db5f3a3b49fdbd745240483ce30dd
foolishflyfox <fenghuabin1992@163.com>
2025-12-07 12:01:50 +0800

    feat: 添加背景横线

--- docs/book/html5-canvas/codes/01/SpriteCoordinate.vue
+++ docs/book/html5-canvas/codes/01/SpriteCoordinate.vue
@@ -1,5 +1,11 @@
 <template>
-  <CanvasContainer :width="500" :height="250" :draw />
+  <CanvasContainer
+    :width="500"
+    :height="250"
+    :draw
+    background-color="#fff"
+    style="border: 1px #888 solid"
+  />
 </template>
 
 <script setup lang="ts">
@@ -14,6 +20,25 @@ function draw(context: CanvasRenderingContext2D) {
     context.drawImage(spritesheet, 0, 0);
   }
   spritesheet.src = runningSpriteSheetUrl;
+
+  /**
+   * 绘制作为背景的横线
+   */
+  function drawBackground() {
+    const VERTICAL_LINE_SPACING = 12;
+    let i = canvas.height;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.strokeStyle = "lightgray";
+    context.lineWidth = 0.5;
+    while (i > VERTICAL_LINE_SPACING * 4) {
+      context.beginPath();
+      context.moveTo(0, i);
+      context.lineTo(canvas.width, i);
+      context.stroke();
+      i -= VERTICAL_LINE_SPACING;
+    }
+  }
+  drawBackground();
   spritesheet.onload = function (e) {
     drawSpritesheet();
   };
